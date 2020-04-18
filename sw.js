4da227cf38cc689d7c6278eca23fc0a252daa928
Fernando Leonid <fernandoleonid@gmail.com>
2020-04-18 09:21:07 -0300

    teste instalação do PWA

--- sw.js
+++ sw.js
@@ -1,37 +1,23 @@
-var CACHE_NAME = 'static-v1';
-
-self.addEventListener('install', function (event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('video-store').then(function(cache) {
       return cache.addAll([
         '/',
         '/index.html',
         '/css/reset.css',
-        '/css/styles.css'
+        '/css/style.css',
+        '/img/icon.png',
+        '/img/icon144.png'
       ]);
     })
-  )
-});
-
-self.addEventListener('activate', function activator(event) {
-  event.waitUntil(
-    caches.keys().then(function (keys) {
-      return Promise.all(keys
-        .filter(function (key) {
-          return key.indexOf(CACHE_NAME) !== 0;
-        })
-        .map(function (key) {
-          return caches.delete(key);
-        })
-      );
-    })
   );
-});
-
-self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.match(event.request).then(function (cachedResponse) {
-      return cachedResponse || fetch(event.request);
-    })
-  );
-});
+ });
+ 
+ self.addEventListener('fetch', function(e) {
+   console.log(e.request.url);
+   e.respondWith(
+     caches.match(e.request).then(function(response) {
+       return response || fetch(e.request);
+     })
+   );
+ });
